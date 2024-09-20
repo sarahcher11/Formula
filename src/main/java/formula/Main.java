ca68a6386f2145df954ef807f2c2dd2c641520e8
c23025119 <sarah.cherchem@etu.univ-amu.fr>
2024-09-20 10:56:26 +0200

    Complete the main methode to test the code

--- src/main/java/formula/Main.java
+++ src/main/java/formula/Main.java
@@ -2,5 +2,14 @@ package formula;
 
 public class Main {
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Variable x = new Variable("x", 2.5);
+        Variable y = new Variable("y", 4);
+        Formula formula =
+                new Sum(x, new Product(y, new Sum(x, y)));
+        System.out.println(formula.asString()); // "(x+(y*(x+y))"
+        System.out.println(formula.asValue()); // "28.5"
+        x.set(5);
+        System.out.println(formula.asValue()); // "41.0"
+    }
 }
