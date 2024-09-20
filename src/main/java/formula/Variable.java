3b0def80446924429cc8c7c061f98d37b46b9880
c23025119 <sarah.cherchem@etu.univ-amu.fr>
2024-09-20 10:29:31 +0200

    Completer le constructeur Variable

--- src/main/java/formula/Variable.java
+++ src/main/java/formula/Variable.java
@@ -1,6 +1,11 @@
 package formula;
 
 public class Variable implements Formula {
+
+    private String variable ;
+    private double value;
     public Variable(String variable, double i) {
+        this.variable=variable;
+        this.value=i;
     }
 }
