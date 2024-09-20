cc73f8de6571f858a457e15c43b8efbdedda13a5
c23025119 <sarah.cherchem@etu.univ-amu.fr>
2024-09-20 11:15:46 +0200

    Power : add the constructor

--- src/main/java/formula/Power.java
+++ src/main/java/formula/Power.java
@@ -3,6 +3,10 @@ package formula;
 public class Power implements Formula {
     private Formula formula;
     private double exponent;
+    public Power(Formula formula, double exponent) {
+        this.formula = formula;
+        this.exponent = exponent;
+    }
     @Override
     public String asString() {
         return "";
