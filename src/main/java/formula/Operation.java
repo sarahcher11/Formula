0e7347578dac355728f86d68d96245c55a990348
c23025119 <sarah.cherchem@etu.univ-amu.fr>
2024-09-20 11:58:54 +0200

    Operation : redefine the methode asString()

--- src/main/java/formula/Operation.java
+++ src/main/java/formula/Operation.java
@@ -8,5 +8,17 @@ public abstract class  Operation implements Formula {
     }
     protected abstract String symbol();
 
+    @Override
+    public String asString() {
+        StringBuilder sb = new StringBuilder("(");
+        for (int i = 0; i < formulas.length; i++) {
+            sb.append(formulas[i].asString());
+            if (i < formulas.length - 1) {
+                sb.append(symbol());
+            }
+        }
+        sb.append(")");
+        return sb.toString();
+    }
 
 }
