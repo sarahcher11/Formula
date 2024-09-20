65708458f11679883b5556db8d283c32f70c5b33
c23025119 <sarah.cherchem@etu.univ-amu.fr>
2024-09-20 11:15:24 +0200

    Power : define the variables of the class

--- src/main/java/formula/Power.java
+++ src/main/java/formula/Power.java
@@ -1,4 +1,15 @@
 package formula;
 
-public class Power {
+public class Power implements Formula {
+    private Formula formula;
+    private double exponent;
+    @Override
+    public String asString() {
+        return "";
+    }
+
+    @Override
+    public double asValue() {
+        return 0;
+    }
 }
