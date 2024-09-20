d79eae9ee874dfb53c336e5cf7716f56eba35909
c23025119 <sarah.cherchem@etu.univ-amu.fr>
2024-09-20 11:21:31 +0200

    Minimum : create the constructor of the class

--- src/main/java/formula/Minimum.java
+++ src/main/java/formula/Minimum.java
@@ -1,6 +1,11 @@
 package formula;
 
 public class Minimum implements Formula{
+    private Formula[] formulas;
+
+    public Minimum(Formula... formulas) {
+        this.formulas = formulas;
+    }
     @Override
     public String asString() {
         return "";
