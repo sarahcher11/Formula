26125a21190175ee8e9bb3448f8b4599ad755179
c23025119 <sarah.cherchem@etu.univ-amu.fr>
2024-09-20 10:49:55 +0200

    Override the methode asString() in the class Product

--- src/main/java/formula/Product.java
+++ src/main/java/formula/Product.java
@@ -1,9 +1,27 @@
 package formula;
 
-public class Product {
+public class Product implements Formula{
     private Formula[] factors;
 
     public Product(Formula... factors) {
         this.factors = factors;
+    }
+
+    @Override
+    public String asString() {
+        StringBuilder sb = new StringBuilder("(");
+        for (int i = 0; i < factors.length; i++) {
+            sb.append(factors[i].asString());
+            if (i < factors.length - 1) {
+                sb.append("*");
+            }
+        }
+        sb.append(")");
+        return sb.toString();
+    }
+
+    @Override
+    public double asValue() {
+        return 0;
     }
 }
