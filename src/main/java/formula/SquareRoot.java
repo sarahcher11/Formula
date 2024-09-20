d975cf34595c99d478913c40b92588822b468596
c23025119 <sarah.cherchem@etu.univ-amu.fr>
2024-09-20 11:11:02 +0200

    SquareRoot : Create the constructor

--- src/main/java/formula/SquareRoot.java
+++ src/main/java/formula/SquareRoot.java
@@ -1,4 +1,17 @@
 package formula;
 
-public class SquareRoot {
+public class SquareRoot implements Formula{
+    private String formula;
+    public SquareRoot(String formula) {
+        this.formula = formula;
+    }
+    @Override
+    public String asString() {
+        return "";
+    }
+
+    @Override
+    public double asValue() {
+        return 0;
+    }
 }
