03f3c6d6e835558a4b30a019735e7e9fe2c3eaf1
c23025119 <sarah.cherchem@etu.univ-amu.fr>
2024-09-20 11:20:57 +0200

    CReate the class Minimum

---
+++ src/main/java/formula/Minimum.java
@@ -0,0 +1,13 @@
+package formula;
+
+public class Minimum implements Formula{
+    @Override
+    public String asString() {
+        return "";
+    }
+
+    @Override
+    public double asValue() {
+        return 0;
+    }
+}
