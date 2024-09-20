eb092f4177cc7e8846b68ddabafc5af417f62614
c23025119 <sarah.cherchem@etu.univ-amu.fr>
2024-09-20 11:02:07 +0200

    AbsoluteValue : add the constructor

--- src/main/java/formula/AbsoluteValue.java
+++ src/main/java/formula/AbsoluteValue.java
@@ -1,4 +1,19 @@
 package formula;
 
-public class AbsoluteValue {
+public class AbsoluteValue implements Formula {
+
+    private Formula formula;
+
+    public AbsoluteValue(Formula formula) {
+        this.formula = formula;
+    }
+    @Override
+    public String asString() {
+        return "";
+    }
+
+    @Override
+    public double asValue() {
+        return 0;
+    }
 }
