238cbccd37d610c76214a2ae8e3d9bb5255d2e35
c23025119 <sarah.cherchem@etu.univ-amu.fr>
2024-09-25 12:16:06 +0200

    VariadicOperator : add the constructor

--- src/main/java/formula/VariadicOperator.java
+++ src/main/java/formula/VariadicOperator.java
@@ -4,4 +4,7 @@ public abstract class VariadicOperator implements Formula{
 
     protected Formula[] formulas;
 
+    public VariadicOperator(Formula[] formulas) {
+        this.formulas = formulas;
+    }
 }
