a87fcc0a64233668f8b80e15716978c115e8e3f9
c23025119 <sarah.cherchem@etu.univ-amu.fr>
2024-09-25 12:21:32 +0200

    VariadicOperator : add asString()

--- src/main/java/formula/VariadicOperator.java
+++ src/main/java/formula/VariadicOperator.java
@@ -7,4 +7,17 @@ public abstract class VariadicOperator implements Formula{
     public VariadicOperator(Formula[] formulas) {
         this.formulas = formulas;
     }
+
+    @Override
+    public String asString() {
+        StringBuilder sb = new StringBuilder("(");
+        for (int i = 0; i < formulas.length; i++) {
+            sb.append(formulas[i].asString());
+            if (i < formulas.length - 1) {
+                sb.append(symbol());
+            }
+        }
+        sb.append(")");
+        return sb.toString();
+    }
 }
