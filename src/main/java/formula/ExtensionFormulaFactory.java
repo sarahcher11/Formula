d7abe6ca0000cbcea5a23bfcd616182779d3b97d
Sarah CHERCHEM <ls_cherchem@esi.dz>
2024-10-04 20:11:24 +0200

    ExtensionFormulaFactory : complete the methode createSum

--- src/main/java/formula/ExtensionFormulaFactory.java
+++ src/main/java/formula/ExtensionFormulaFactory.java
@@ -8,11 +8,11 @@ public class ExtensionFormulaFactory implements AbstractFormulaFactory {
 
     @Override
     public Formula createProduct(Formula... formulas) {
-        return new Sum(formulas);
+        return new Product(formulas);
     }
 
     @Override
     public Formula createSum(Formula... formulas) {
-        return null;
+        return new Sum(formulas);
     }
 }
