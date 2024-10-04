df733f47c2b1a576a311b2b457e24ae6e4c2d59a
Sarah CHERCHEM <ls_cherchem@esi.dz>
2024-10-04 19:20:12 +0200

    AbstractFormulaFactory : add the methode createConstant , createProduct, createSum

--- src/main/java/formula/AbstractFormulaFactory.java
+++ src/main/java/formula/AbstractFormulaFactory.java
@@ -1,5 +1,9 @@
 package formula;
 
+import java.text.Normalizer;
+
 public interface AbstractFormulaFactory {
     public Formula createConstant(double Value);
+    public Formula createProduct(Formula... formulas);
+    public Formula createSum(Formula... formulas);
 }
