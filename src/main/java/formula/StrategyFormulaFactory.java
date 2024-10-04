999443e203dd3c7da670854b96c59b246070e3b5
Sarah CHERCHEM <ls_cherchem@esi.dz>
2024-10-04 19:34:57 +0200

    StrategyFormulaFactory : complete the methode createConstant

--- src/main/java/formula/StrategyFormulaFactory.java
+++ src/main/java/formula/StrategyFormulaFactory.java
@@ -1,4 +1,18 @@
 package formula;
 
-public class StrategyFormulaFactory {
+public class StrategyFormulaFactory implements AbstractFormulaFactory{
+    @Override
+    public Formula createConstant(double value) {
+        return new Constant(value);
+    }
+
+    @Override
+    public Formula createProduct(Formula... formulas) {
+        return null;
+    }
+
+    @Override
+    public Formula createSum(Formula... formulas) {
+        return null;
+    }
 }
