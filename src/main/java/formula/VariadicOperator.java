a98a415cd302613e8c799e832232d729e8045abd
Sarah CHERCHEM <ls_cherchem@esi.dz>
2024-10-04 18:30:13 +0200

    VariadicOperator : make the class normal class and add the variable Operator

--- src/main/java/formula/VariadicOperator.java
+++ src/main/java/formula/VariadicOperator.java
@@ -1,25 +1,22 @@
 package formula;
 
-public abstract class VariadicOperator implements Formula {
+public  class VariadicOperator implements Formula {
 
-    protected Formula[] formulas;
+    private Formula[] formulas;
+    private Operator operator ;
 
-    public VariadicOperator(Formula[] formulas) {
+    public VariadicOperator(Operator operator, Formula[] formulas) {
+
         this.formulas = formulas;
+        this.operator=operator;
     }
-    // Méthode abstraite pour définir le symbole (à implémenter dans les sous-classes)
-    protected abstract String symbol();
-    // Méthode abstraite pour initialiser la valeur de départ (ex: 0 pour Sum, 1 pour Product)
-    protected abstract double initialValue();
-    // Méthode abstraite pour définir la manière de combiner les valeurs (addition pour Sum, multiplication pour Product)
-    protected abstract double cumulativeValue(double a, double b);
     @Override
     public String asString() {
         StringBuilder sb = new StringBuilder("(");
         for (int i = 0; i < formulas.length; i++) {
             sb.append(formulas[i].asString());
             if (i < formulas.length - 1) {
-                sb.append(symbol());
+                sb.append(operator.symbol());
             }
         }
         sb.append(")");
@@ -27,9 +24,9 @@ public abstract class VariadicOperator implements Formula {
     }
     @Override
     public double asValue() {
-        double result = initialValue();
+        double result = operator.initialValue();
         for (Formula formula : formulas) {
-            result = cumulativeValue(result, formula.asValue());
+            result = operator.cumulativeValue(result, formula.asValue());
         }
         return result;
     }
