c94b3861047d6dd58286864441096eede7a305d6
c23025119 <sarah.cherchem@etu.univ-amu.fr>
2024-10-04 10:54:50 +0200

    Constant : add the constructor

--- src/main/java/formula/Constant.java
+++ src/main/java/formula/Constant.java
@@ -1,4 +1,20 @@
 package formula;
 
-public class Constant {
+public class Constant implements Formula{
+
+    int value;
+
+    public Constant(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String asString() {
+        return "";
+    }
+
+    @Override
+    public double asValue() {
+        return 0;
+    }
 }
