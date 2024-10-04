847e33441365bb27b55a060c27723197d0f28f36
Sarah CHERCHEM <ls_cherchem@esi.dz>
2024-10-04 19:49:24 +0200

    VariadicOperatorStrategy : complete the class

--- src/main/java/formula/VariadicOperatorStrategy.java
+++ src/main/java/formula/VariadicOperatorStrategy.java
@@ -1,4 +1,32 @@
 package formula;
 
-public class VariadicOperatorStrategy {
+public class VariadicOperatorStrategy implements Formula{
+
+
+    private Formula[] formulas;
+    private Operator operator;
+
+    public VariadicOperatorStrategy(Formula[] formulas, Operator operator) {
+        this.formulas = formulas;
+        this.operator = operator;
+    }
+
+    @Override
+    public double asValue() {
+        double value = operator.initialValue();
+        for (Formula formula : formulas)
+            value = operator.cumulativeValue(formula.asValue(), value);
+        return value;
+    }
+
+
+
+    @Override
+    public String asString() {
+        String string = "(" + formulas[0].asString();
+        for (int index = 1; index < formulas.length; index++)
+            string = string + operator.symbol() + formulas[index].asString();
+        return string + ")";
+    }
+
 }
