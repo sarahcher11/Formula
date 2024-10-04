2b78331231d45c23325b8a130c09be72101699e9
Sarah CHERCHEM <ls_cherchem@esi.dz>
2024-10-04 20:09:34 +0200

    ExtensionFormulaFactory : complete the class

--- src/main/java/formula/ExtensionFormulaFactory.java
+++ src/main/java/formula/ExtensionFormulaFactory.java
@@ -1,4 +1,18 @@
 package formula;
 
-public class ExtensionFormulaFactory {
+public class ExtensionFormulaFactory implements AbstractFormulaFactory {
+    @Override
+    public Formula createConstant(double Value) {
+        return new Constant(Value);
+    }
+
+    @Override
+    public Formula createProduct(Formula... formulas) {
+        return null;
+    }
+
+    @Override
+    public Formula createSum(Formula... formulas) {
+        return null;
+    }
 }
