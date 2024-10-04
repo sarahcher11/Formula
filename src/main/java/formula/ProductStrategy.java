d9f4c19d9d6c32617f3e7f01ed78f82f02e82437
Sarah CHERCHEM <ls_cherchem@esi.dz>
2024-10-04 19:28:11 +0200

    ProductStrategy : complete the class implementing the methode of the interface Operator

--- src/main/java/formula/ProductStrategy.java
+++ src/main/java/formula/ProductStrategy.java
@@ -1,4 +1,18 @@
 package formula;
 
-public class ProductStrategy {
+public class ProductStrategy implements Operator {
+    public String symbol() {
+        return "+";
+    }
+
+    @Override
+    public double initialValue() {
+        return 0; // La somme commence à 0
+    }
+
+    @Override
+    public double cumulativeValue(double accumulator, double value) {
+        return accumulator + value; // Addition des valeurs
+
+    }
 }
